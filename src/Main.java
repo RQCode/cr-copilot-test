c9924f528f676c868c80476bbc6c8adad5e2620d
RQCode <13011030221@163.com>
2025-02-10 21:31:12 +0800

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        while (true) {
+        int i = 0;
+        while (i < 10) {
             System.out.println("Hello1, World!");
+            i++;
         }
     }
 }
